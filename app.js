95394e8797228662558a0f4afedb6e2a8dc0da0b
Revamth <rvrevamth@gmail.com>
2025-01-26 22:30:33 +0530

    CDN import

---
+++ app.js
@@ -0,0 +1,18 @@
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello World from React!"
+);
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "children" }, [
+    React.createElement("h1", { id: "heading" }, "Hello from child"),
+    React.createElement("h1", { id: "heading" }, "Hello from child2"),
+  ])
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
