2c9e5a8bd837ef530975b5939de0239cf3ddd7a7
Revamth <rvrevamth@gmail.com>
2025-01-30 23:21:25 +0530

    creating react hooks

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,26 @@
 import Restrocard from "./Restocard";
 import resList from "../utils/resList";
+import { useState } from "react";
 
 const Body = () => {
+  const [listofRestaurants, setlistofRestaurants] = useState(resList);
   return (
     <div className="body">
-      <div className="search"></div>
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = listofRestaurants.filter(
+              (restauant) => restauant.info.avgRating > 4
+            );
+            setlistofRestaurants(filteredList);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
       <div className="restro-container">
-        {resList.map((restauant) => (
+        {listofRestaurants.map((restauant) => (
           <Restrocard key={restauant.info.id} resData={restauant} />
         ))}
       </div>
