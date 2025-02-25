2d33c2be18688401ddda9d396db06bf565773686
Revamth <rvrevamth@gmail.com>
2025-02-25 14:42:52 +0530

    applied cors proxy using thingproxy

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -5,11 +5,11 @@ export const LOGO_URL =
   "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKGR_e0KO1_QlnsND7ypy4L1uulHSuVg-TVQ&s";
 
 export const MENU_URL =
-  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.5204303&lng=73.8567437&restaurantId=";
+  "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.5204303&lng=73.8567437&restaurantId=";
 
 // utils/constants.js
 export const SWIGGY_API_URL =
-  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.5204303&lng=73.8567437&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+  "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.5204303&lng=73.8567437&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
 
 export const CART_IMAGE =
   "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9r-ZOowxbdfG2Q0cVgZfm6PWoTI-YFgwQlw&s";
